400c6c8e16d53ce7126fc91a6da2e9b1819ddc1b
ksw <wonie6381@gmail.com>
2025-09-29 17:50:37 +0900

    v0.5 types/main.go

--- week06/types/main.go
+++ week06/types/main.go
@@ -1,10 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"reflect"
-)
-
 func main() {
 	// fmt.Println(math.Round(2.21))
 	// fmt.Println(math.Ceil(2.21))
@@ -30,10 +25,22 @@ func main() {
 	// var name string = "kimsangwon"
 	// var gpa float32 = 3.99
 
-	id := 999
-	name := "kimsangwon"
-	gpa := 3.99
+	// id := 999
+	// name := "kimsangwon"
+	// gpa := 3.99
 
-	fmt.Println("학번은", id, reflect.TypeOf(id), ", 이름은", name, reflect.TypeOf(name))
-	fmt.Println("평점 : ", gpa, reflect.TypeOf(gpa))
+	// fmt.Println("학번은", id, reflect.TypeOf(id), ", 이름은", name, reflect.TypeOf(name))
+	// fmt.Println("평점 : ", gpa, reflect.TypeOf(gpa))
+
+	// var f64 float64
+	// var t bool
+	// var s string
+	// var i int
+	// var i16 int16
+
+	// fmt.Println(f64, reflect.TypeOf(f64))
+	// fmt.Println(t, reflect.TypeOf(t))
+	// fmt.Println(s, reflect.TypeOf(s))
+	// fmt.Println(i, reflect.TypeOf(i))
+	// fmt.Println(i16, reflect.TypeOf(i16))
 }
