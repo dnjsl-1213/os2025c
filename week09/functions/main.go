02ddb463e9adb869803d5e2241f0edde38083986
test <test@test.com>
2025-10-27 18:06:55 +0900

    functions/main.go v0.2 package

--- week09/functions/main.go
+++ week09/functions/main.go
@@ -1,20 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
 
-func swap(first *int, second *int) {
-	var temp int = 0
-	temp = *first
-	*first = *second
-	*second = temp
-	fmt.Println(*first, *second)
+func GetFloat() (float64, error) {
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		return 0, err
+	}
+
+	input = strings.TrimSpace(input)
+	number, err := strconv.ParseFloat(input, 64)
+	if err != nil {
+		return 0, err
+	}
+	return number, nil
 }
 
 func main() {
-	a, b := 10, 20
-	fmt.Println(a, b)
-	swap(&a, &b)
-	fmt.Println(a, b)
+	fmt.Print("점수 입력 : ")
+	score, err := GetFloat()
+	if err != nil {
+		log.Fatal(err)
+	}
+	passFail := ""
+	if score >= 60 {
+		passFail = "합격"
+	} else {
+		passFail = "불합격"
+	}
+	fmt.Printf("%0.2f점은 %v\n", score, passFail)
+
+	// a, b := 10, 20
+	// fmt.Println(a, b)
+	// swap(&a, &b)
+	// fmt.Println(a, b)
+
 	// fmt.Print("%0.3f\n", math.Sqrt(-9.3))
 
 }
