c2e87a7a7dfd0d1c0d09e0240344851a8eb003c7
test <test@test.com>
2025-11-03 18:23:10 +0900

    week10 array v0.5

--- week10/cmd/ch05-ex01/main.go
+++ week10/cmd/ch05-ex01/main.go
@@ -15,11 +15,13 @@ func main() {
 	// arrayInt[1] = 2
 	// fmt.Println(arrayInt[1])
 
-	arrayBool := [2]bool{true, false}
-	arrayInt := [3]int{-9, 11, 7}
-	for i := 0; i < len(arrayInt); i++ {
-		fmt.Println(i, arrayBool[i])
-		fmt.Println(i, arrayInt[i])
-	}
+	// arrayInt := [3]int{-9, 11, 7}
+	// for i := 0; i < len(arrayInt); i++ {
+	// 	fmt.Println(i, arrayInt[i])
+	// }
 
+	numbers := [3]int{-9, 11, 7}
+	for i, number := range numbers {
+		fmt.Println(i, number)
+	}
 }
