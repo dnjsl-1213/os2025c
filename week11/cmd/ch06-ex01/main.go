16595ba2b23eabe992e78e01856d574b469594c1
test <test@test.com>
2025-11-10 17:44:20 +0900

    week11 slice v0.6

--- week11/cmd/ch06-ex01/main.go
+++ week11/cmd/ch06-ex01/main.go
@@ -5,8 +5,9 @@ import "fmt"
 func main() {
 	subjects := []string{"Go", "javascript", "Python", "Linux"}
 	subjectsSlice := subjects[:3] // slicing
-	//subjects[0] = "Java"
-	subjectsSlice[0] = "Database"
+	subjects[0] = "Java"
+	subjectsSlice = append(subjectsSlice, "Go") // python : subjectsSlice.append("Go")
+	//subjectsSlice = append(subjectsSlice, "Go", "DB") // 배열의 길이를 오버하면 다른 메모리로 할당
 	for _, subject := range subjects {
 		fmt.Println(subject)
 	}
